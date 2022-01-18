e511b849b85b1005e4cc48a2f4a233559a57c9a2
Maryna Danylchuk <marina123456mari@gmail.com>
2022-01-18 13:02:33 +0200

    hw02

--- model/index.js
+++ model/index.js
@@ -1,15 +1,55 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require("fs/promises");
+const { v4 } = require("uuid");
 
-const listContacts = async () => {}
+const contactsPath = require("../routes/api/contacts/contactsPath");
 
-const getContactById = async (contactId) => {}
+const getAll = require("../routes/api/contacts/getAll");
+const updateContacts = require("../routes/api/contacts/updateContacts");
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+};
 
-const addContact = async (body) => {}
+const getContactById = async (id) => {
+  const contacts = await getAll();
+  const result = contacts.find((item) => item.id === id);
+  if (!result) {
+    return null;
+  }
+  return result;
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (id) => {
+  const contacts = await getAll();
+  const idx = contacts.findIndex((item) => item.id === id);
+  if (idx === -1) {
+    return null;
+  }
+  const newContacts = contacts.filter((_, index) => index !== idx);
+  await updateContacts(newContacts);
+  return contacts[idx];
+};
+
+const addContact = async (body) => {
+  const newContacts = { ...body, id: v4() };
+  const products = await getAll();
+  products.push(newContacts);
+  await updateContacts(products);
+  return newContacts;
+};
+
+const updateContact = async (id, body) => {
+  const contacts = await getAll();
+  const idx = contacts.findIndex((item) => item.id === id);
+  if (idx === -1) {
+    return null;
+  }
+  contacts[idx] = { ...body, id };
+  await updateContacts(contacts);
+  return contacts[idx];
+};
 
 module.exports = {
   listContacts,
@@ -17,4 +57,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
