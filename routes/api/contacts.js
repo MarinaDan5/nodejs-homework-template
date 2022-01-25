e693061b2412f3c5420ce583148a449a93d3cc71
Maryna Danylchuk <marina123456mari@gmail.com>
2022-01-25 17:40:40 +0200

    hw4

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,19 +3,30 @@ const express = require("express");
 const createError = require("http-errors");
 const router = express.Router();
 
+const { authenticate } = require("../../middlewares");
 const { Contact } = require("../../model");
 const { joiSchema } = require("../../model/contact");
 
-router.get("/", async (req, res, next) => {
+router.get("/", authenticate, async (req, res, next) => {
   try {
-    const contacts = await Contact.find();
+    const { page = 1, limit = 10 } = req.query;
+    const { _id } = req.user;
+    const skip = (page - 1) * limit;
+    const contacts = await Contact.find(
+      { owner: _id },
+      "-createdAt -updatedAt",
+      {
+        skip,
+        limit: +limit,
+      }
+    );
     res.json(contacts);
   } catch (error) {
     next(error);
   }
 });
 
-router.get("/:id", async (req, res, next) => {
+router.get("/:id", authenticate, async (req, res, next) => {
   const { id } = req.params;
   try {
     const contact = await Contact.findById(id);
@@ -31,14 +42,15 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
-router.post("/", async (req, res, next) => {
+router.post("/", authenticate, async (req, res, next) => {
   try {
     const { error } = joiSchema.validate(req.body);
     if (error) {
       error.status = 400;
       throw error;
     }
-    const newContact = await Contact.create(req.body);
+    const { _id } = req.user;
+    const newContact = await Contact.create({ ...req.body, owner: _id });
     res.status(201).json(newContact);
   } catch (error) {
     if (error.message.includes("validation failed")) {
